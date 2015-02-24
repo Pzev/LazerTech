5b38900f8f3feedc00a1c9829b9bd152779546a4
Pzev <pzev77@gmail.com>
2015-02-23 20:07:55 -0500

    Update
    All needed items to start building the mod, and allow it to run in fml

---
+++ src/main/java/com/pzev/lazertech/LazerTech.java
@@ -0,0 +1,29 @@
+package com.pzev.lazertech;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="LazerTech", name="LazerTech", version="1.7.10-1.0")
+
+public class LazerTech {
+    @Mod.Instance("LazerTech")
+    public static LazerTech instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+
+}
