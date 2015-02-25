75f1749de1ec660290a8a8b906b27bd035d7c552
Pzev <pzev77@gmail.com>
2015-02-25 18:23:56 -0500

    added pliers and hammer texture

--- src/main/java/com/pzev/lazertech/init/ModItems.java
+++ src/main/java/com/pzev/lazertech/init/ModItems.java
@@ -1,10 +1,6 @@
 package com.pzev.lazertech.init;
 
-import com.pzev.lazertech.item.ItemElectricSpear;
-import com.pzev.lazertech.item.ItemHammer;
-import com.pzev.lazertech.item.ItemLT;
-import com.pzev.lazertech.item.ItemPewPewGun;
-import com.pzev.lazertech.item.ItemPowPowGun;
+import com.pzev.lazertech.item.*;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ModItems
@@ -12,13 +8,16 @@ public class ModItems
     public static final ItemLT pewPewGun = new ItemPewPewGun();
     public static final ItemLT powPowGun = new ItemPowPowGun();
     public static final ItemLT electricSpear = new ItemElectricSpear();
-public static final ItemLT hammer = new ItemHammer();
+    public static final ItemLT hammer = new ItemHammer();
+    public static final ItemLT pliers = new ItemPliers();
 
     public static void init()
     {
         GameRegistry.registerItem(pewPewGun, "pewPewGun");
         GameRegistry.registerItem(powPowGun, "powPowGun");
         GameRegistry.registerItem(electricSpear, "electricSpear");
-GameRegistry.registerItem(hammer, "hammer");
+        GameRegistry.registerItem(hammer, "hammer");
+        GameRegistry.registerItem(pliers, "pliers");
+
     }
 }
---
+++ src/main/java/com/pzev/lazertech/item/ItemPliers.java
@@ -0,0 +1,11 @@
+package com.pzev.lazertech.item;
+
+public class ItemPliers extends ItemLT
+{
+    public ItemPliers()
+    {
+        super();
+        this.setUnlocalizedName("pliers");
+    }
+
+}
