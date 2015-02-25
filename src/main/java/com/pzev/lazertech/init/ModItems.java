e955cf8d67020c72302f1b31eef8eac1c31e2ef5
Pzev <pzev77@gmail.com>
2015-02-25 08:10:58 -0500

    Update ModItems.java

--- src/main/java/com/pzev/lazertech/init/ModItems.java
+++ src/main/java/com/pzev/lazertech/init/ModItems.java
@@ -1,6 +1,7 @@
 package com.pzev.lazertech.init;
 
 import com.pzev.lazertech.item.ItemElectricSpear;
+import com.pzev.lazertech.item.ItemHammer;
 import com.pzev.lazertech.item.ItemLT;
 import com.pzev.lazertech.item.ItemPewPewGun;
 import com.pzev.lazertech.item.ItemPowPowGun;
@@ -11,11 +12,13 @@ public class ModItems
     public static final ItemLT pewPewGun = new ItemPewPewGun();
     public static final ItemLT powPowGun = new ItemPowPowGun();
     public static final ItemLT electricSpear = new ItemElectricSpear();
+public static final ItemLT hammer = new ItemHammer();
 
     public static void init()
     {
         GameRegistry.registerItem(pewPewGun, "pewPewGun");
         GameRegistry.registerItem(powPowGun, "powPowGun");
         GameRegistry.registerItem(electricSpear, "electricSpear");
+GameRegistry.registerItem(hammer, "hammer");
     }
 }
