238f36664d06f9f529ff647638ec26abbf7e0d4e
TheCricket <bcord@me.com>
2015-07-31 12:45:13 -0400

    I don't even know

--- src/main/java/com/pzev/lazertech/client/render/RenderLazerFurnace.java
+++ src/main/java/com/pzev/lazertech/client/render/RenderLazerFurnace.java
@@ -23,44 +23,25 @@ public class RenderLazerFurnace extends TileEntitySpecialRenderer {
         this.model = new ModelElectricFurnace();
     }
 
-    private void adjustRotatePivotMeta(World world, int x, int y, int z) {
-        int meta = world.getBlockMetadata(x, y, z);
-        GL11.glPushMatrix();
-        GL11.glRotatef(meta * (-90), 0.0F, 0.0F, 1.0F);
-        GL11.glPopMatrix();
-    }
-
     @Override
     public void renderTileEntityAt(TileEntity tile, double x, double y, double z, float scale) {
+        if(tile instanceof TileEntityLaserFurnace)
+            this.renderLazerFurnaceAt((TileEntityLaserFurnace) tile, x, y, z, scale);
+    }
+
+    protected void renderLazerFurnaceAt(TileEntityLaserFurnace furnace, double x, double y, double z, float scale) {
         GL11.glPushMatrix();
-        GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) + 0.5F);
         ResourceLocation off = new ResourceLocation(Reference.MOD_ID + ":textures/blocks/laserFurnace.png");
         ResourceLocation on = new ResourceLocation(Reference.MOD_ID + ":textures/blocks/laserFurnace1.png");
 
-        if(tile instanceof TileEntityLaserFurnace) {
-            TileEntityLaserFurnace furnace = (TileEntityLaserFurnace) tile;
-
-            if(furnace.isSmelting()) {
-                Minecraft.getMinecraft().renderEngine.bindTexture(on);
-            } else {
-                Minecraft.getMinecraft().renderEngine.bindTexture(off);
-            }
-
-            GL11.glPushMatrix();
-            GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
-            this.model.render((Entity) null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
-            GL11.glPopMatrix();
+        if(furnace.isSmelting()) {
+            Minecraft.getMinecraft().renderEngine.bindTexture(on);
+        } else {
+            Minecraft.getMinecraft().renderEngine.bindTexture(off);
         }
-        GL11.glPopMatrix();
-    }
 
-    private void adjustLightFixture(World world, int x, int y, int z, Block block) {
-        Tessellator tess = Tessellator.instance;
-        float brightness = block.getLightValue(world, x, y, z);
-        int skyLight = world.getLightBrightnessForSkyBlocks(x, y, z, 0);
-        int modulousModifier = skyLight % 65536;
-        int divModifier = skyLight / 65535;
-        tess.setColorOpaque_F(brightness, brightness, brightness);
-        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float) modulousModifier, divModifier);
+        GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
+        this.model.render((Entity) null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F); //Why is this binding to the player
+        GL11.glPopMatrix();
     }
 }
